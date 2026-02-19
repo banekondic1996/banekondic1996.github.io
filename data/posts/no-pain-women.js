7e70338e404c1a4b27b09b7d30b363a34a54b71b
Branislav (Bane) Kondic <101-2015@ftn.edu.rs>
2026-02-19 12:14:14 +0100

    Add files via upload

---
+++ data/posts/no-pain-women.js
@@ -0,0 +1,4 @@
+     
+POST_CONTENT = {
+ "content": "<iframe width=\"100%\" height=\"400wh\" src=\"https://www.youtube-nocookie.com/embed/qp0y78VLwcQ\" title=\"YouTube video\" allow=\"accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture\" allowfullscreen=\"\">\n</iframe>\n\n"
+};
